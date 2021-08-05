2b0a0899716bfe2b5124bfd76a7e5279c7694996
Nartovdima <61012787+Nartovdima@users.noreply.github.com>
2021-08-05 14:14:06 +0300

    Create inverse_element.cpp

---
+++ number_theory/inverse_element/inverse_element.cpp
@@ -0,0 +1,133 @@
+#include <unordered_set>
+#include <unordered_map>
+//#include <ext/rope>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <iomanip>
+#include <fstream>
+#include <cassert>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <cstdio>
+#include <random>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <ctime>
+#include <map>
+#include <set>
+
+//using namespace __gnu_cxx;
+using namespace std;
+
+/*#pragma GCC optimize("Ofast")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native")
+#pragma GCC optimize("fast-math")*/
+
+typedef long long ll;   
+typedef long double ld;
+typedef pair <int, int> ii;
+typedef pair <ll, ll> dl;
+typedef vector <vector <int> > graph;
+
+#define pb push_back
+#define eb emplace_back 
+#define ff first
+#define ss second
+#define show(x) cerr << (#x) << "=" << x << '\n'
+#define ptree_node tree_node*
+#define sqr(a) ((a) * (a))
+#define all(x) (x).begin(), (x).end()
+ 
+const int INF = 1e9;
+const ld EPS = 1e-8;
+const ld PI = atan2(0.0, -1.0);
+const int M = 1e9;
+const int MAXN = 2 * 1e5;
+
+#ifdef _LOCAL
+    mt19937 rnd(223);
+    mt19937_64 rndll(231);
+#else
+    mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+    mt19937_64 rndll(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+
+template<typename T>             istream& operator>>(istream& is,  vector<T> &v){for (auto& i : v) is >> i;                       return is;}
+template<typename T>             ostream& operator<<(ostream& os,  vector<T>  v){for (auto& i : v) os << i << ' ';                return os;}
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U> &p){is >> p.first >> p.second;                       return is;}
+template<typename T, typename U> ostream& operator<<(ostream& os, pair<T, U>  p){os << '(' << p.first << "; " << p.second << ')'; return os;}
+
+/*const int MAX_MEM = 1e8; 
+int mpos = 0; 
+char mem[MAX_MEM]; 
+inline void * operator new ( size_t n ) { 
+    assert((mpos += n) <= MAX_MEM); 
+    return (void *)(mem + mpos - n); 
+} 
+inline void operator delete (void *) noexcept { } */
+
+#define fast(){ \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0); \
+}
+
+/* --------------------------
+    Solutions starts here!!!
+   -------------------------- */
+
+struct gcd_res {
+    int x;
+    int p;
+    int q;
+
+    gcd_res (int x1 = 0, int p1 = 0, int q1 = 0) : x(x1), p(p1), q(q1) {};
+
+};
+
+gcd_res extended_gcd (int a, int b) {
+    if (b == 0)
+        return gcd_res(a, 1, 0);
+    gcd_res res = extended_gcd(b, a % b);
+    gcd_res new_res (res.x, res.q, res.p - (a / b) * res.q);
+    return new_res;
+}
+
+signed main() {
+    #ifdef _LOCAL
+        clock_t Tsart = clock();
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    fast();
+
+    int a, m;
+    cin >> a >> m;
+
+    gcd_res ans = extended_gcd(a, m);
+    if (ans.x != 1) {
+        cout << "No solution" << '\n';
+        return 0;
+    }
+    
+    cout << (ans.p % m + m) % m << '\n';
+    
+    #ifdef _LOCAL
+        cerr << "Runtime: " << (ld)(clock() - Tsart) / CLOCKS_PER_SEC << '\n';
+    #endif      
+    return 0;
+}
+ 
+/*     .^----^.
+      (= o  O =)
+       (___V__)
+        _|==|_
+   ___/' |--| |
+  / ,._| |  | '
+ | \__ |__}-|__}
+  \___)`*/
