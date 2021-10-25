cde70be9af3799c4d377f2ba897a8a485728d95f
Nartovdima <61012787+Nartovdima@users.noreply.github.com>
2021-10-25 16:29:35 +0300

    Create sparse_table.cpp

---
+++ data_structures/sparse_table/sparse_table.cpp
@@ -0,0 +1,143 @@
+#include <unordered_set>
+#include <unordered_map>
+//#include <ext/rope>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <iomanip>
+#include <fstream>
+#include <cassert>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <cstdio>
+#include <random>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <ctime>
+#include <map>
+#include <set>
+
+//using namespace __gnu_cxx;
+using namespace std;
+
+/*#pragma GCC optimize("Ofast")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native")
+#pragma GCC optimize("fast-math")*/
+
+typedef long long ll;   
+typedef long double ld;
+typedef pair <int, int> ii;
+typedef pair <ll, ll> dl;
+typedef vector <vector <int> > graph;
+
+#define pb push_back
+#define eb emplace_back 
+#define ff first
+#define ss second
+#define show(x) cerr << (#x) << "=" << x << '\n'
+#define ptree_node tree_node*
+#define sqr(a) ((a) * (a))
+#define all(x) (x).begin(), (x).end()
+ 
+const int INF = 1e9;
+const ld EPS = 1e-8;
+const ld PI = atan2(0.0, -1.0);
+const int M = 1e9;
+const int MAXN = 1e5;
+
+#ifdef _LOCAL
+    mt19937 rnd(223);
+    mt19937_64 rndll(231);
+#else
+    mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+    mt19937_64 rndll(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+
+template<typename T>             istream& operator>>(istream& is,  vector<T> &v){for (auto& i : v) is >> i;                       return is;}
+template<typename T>             ostream& operator<<(ostream& os,  vector<T>  v){for (auto& i : v) os << i << ' ';                return os;}
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U> &p){is >> p.first >> p.second;                       return is;}
+template<typename T, typename U> ostream& operator<<(ostream& os, pair<T, U>  p){os << '(' << p.first << "; " << p.second << ')'; return os;}
+
+/*const int MAX_MEM = 1e8; 
+int mpos = 0; 
+char mem[MAX_MEM]; 
+inline void * operator new ( size_t n ) { 
+    assert((mpos += n) <= MAX_MEM); 
+    return (void *)(mem + mpos - n); 
+} 
+inline void operator delete (void *) noexcept { } */
+
+#define fast(){ \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0); \
+}
+
+/* --------------------------
+    Solutions starts here!!!
+   -------------------------- */
+
+
+int st[20][MAXN];
+vector <int> lg;
+
+signed main() {
+    #ifdef _LOCAL
+        clock_t Tsart = clock();
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    fast();
+
+    int n, m;
+    cin >> n >> m;
+
+    lg.resize(n + 1);
+    for (int i = 1, j = 0, g = 2; i <= n; i++) {
+        if (g == i) {
+            g <<= 1;
+            j++;
+        }
+        lg[i] = j;
+    }
+
+    cin >> st[0][0];
+    for (int i = 1; i < n; i++) 
+        st[0][i] = (23 * st[0][i - 1] + 21563) % 16714589;
+
+
+    for (int i = 1; i <= lg[n]; i++)
+        for (int j = 0; j + (1 << i) <= n; j++) 
+            st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+        
+
+    int u, v, ans, g;
+    cin >> u >> v;
+    g = lg[abs(v - u) + 1];
+    ans = min(st[g][min(v, u) - 1], st[g][max(v, u) - (1 << g)]);
+    for (int i = 1; i < m; i++) {
+        u = ((17 * u + 751 + ans + 2 * i) % n) + 1;
+        v = ((13 * v + 593 + ans + 5 * i) % n) + 1;
+        g = lg[abs(v - u) + 1];
+        ans = min(st[g][min(v, u) - 1], st[g][max(v, u) - (1 << g)]);
+    }
+    cout << u << ' ' << v << ' ' << ans << '\n';
+
+    #ifdef _LOCAL
+        cerr << "Runtime: " << (ld)(clock() - Tsart) / CLOCKS_PER_SEC << '\n';
+    #endif      
+    return 0;
+}
+ 
+/*     .^----^.
+      (= o  O =)
+       (___V__)
+        _|==|_
+   ___/' |--| |
+  / ,._| |  | '
+ | \__ |__}-|__}
+  \___)`*/
