b783389b60959bfb4fbe5358b6668527b0369a2c
Nartovdima <61012787+Nartovdima@users.noreply.github.com>
2021-11-18 17:25:49 +0300

    Update disjoint_set_union.cpp

--- data_structures/disjoint_set_union/disjoint_set_union.cpp
+++ data_structures/disjoint_set_union/disjoint_set_union.cpp
@@ -1,1 +1,156 @@
+#include <unordered_set>
+#include <unordered_map>
+//#include <ext/rope>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <iomanip>
+#include <fstream>
+#include <cassert>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <cstdio>
+#include <random>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <ctime>
+#include <map>
+#include <set>
 
+//using namespace __gnu_cxx;
+using namespace std;
+
+/*#pragma GCC optimize("Ofast")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native")
+#pragma GCC optimize("fast-math")*/
+
+typedef long long ll;   
+typedef long double ld;
+typedef pair <int, int> ii;
+typedef pair <ll, ll> dl;
+typedef vector <vector <int> > graph;
+
+#define pb push_back
+#define eb emplace_back 
+#define ff first
+#define ss second
+#define show(x) cerr << (#x) << "=" << x << '\n'
+#define ptree_node tree_node*
+#define sqr(a) ((a) * (a))
+#define all(x) (x).begin(), (x).end()
+ 
+const int INF = 1e9;
+const ld EPS = 1e-8;
+const ld PI = atan2(0.0, -1.0);
+const int M = 1e9;
+const int MAXN = 2 * 1e5;
+
+#ifdef _LOCAL
+    mt19937 rnd(223);
+    mt19937_64 rndll(231);
+#else
+    mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+    mt19937_64 rndll(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+
+template<typename T>             istream& operator>>(istream& is,  vector<T> &v){for (auto& i : v) is >> i;                       return is;}
+template<typename T>             ostream& operator<<(ostream& os,  vector<T>  v){for (auto& i : v) os << i << ' ';                return os;}
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U> &p){is >> p.first >> p.second;                       return is;}
+template<typename T, typename U> ostream& operator<<(ostream& os, pair<T, U>  p){os << '(' << p.first << "; " << p.second << ')'; return os;}
+
+/*const int MAX_MEM = 1e8; 
+int mpos = 0; 
+char mem[MAX_MEM]; 
+inline void * operator new ( size_t n ) { 
+    assert((mpos += n) <= MAX_MEM); 
+    return (void *)(mem + mpos - n); 
+} 
+inline void operator delete (void *) noexcept { } */
+
+#define fast(){ \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0); \
+}
+
+/* --------------------------
+    Solutions starts here!!!
+   -------------------------- */
+
+vector <int> parent, rang;
+
+void make_set (int v) {
+    parent[v] = v;
+    rang[v] = 0;
+}
+
+int find_set (int v) {
+    if (parent[v] == v)
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets (int v, int u) {
+    v = find_set(v);
+    u = find_set(u);
+    if (v != u) {
+        if (rang[v] > rang[u])
+            swap(v, u);
+        parent[v] = u;
+        if (rang[v] == rang[u])
+            rang[u]++;
+    }
+}
+
+signed main() {
+    #ifdef _LOCAL
+        clock_t Tsart = clock();
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    fast();
+
+    int n, m;
+    cin >> n >> m;
+    parent.resize(n + 1, 0);
+    rang.resize(n + 1, -1);
+    while (m--) {
+        string s;
+        int a, b;
+        cin >> s;
+        cin >> a >> b;
+        if (s == "get") {
+            if (parent[a] == 0)
+                make_set(a);
+            if (parent[b] == 0)
+                make_set(b);
+            cout << (find_set(a) == find_set(b) ? "YES" : "NO") << '\n';
+        }
+        else {
+            if (parent[a] == 0)
+                make_set(a);
+            if (parent[b] == 0)
+                make_set(b);
+            union_sets(a, b);
+        }
+
+    }
+    
+    #ifdef _LOCAL
+        cerr << "Runtime: " << (ld)(clock() - Tsart) / CLOCKS_PER_SEC << '\n';
+    #endif      
+    return 0;
+}
+ 
+/*     .^----^.
+      (= o  O =)
+       (___V__)
+        _|==|_
+   ___/' |--| |
+  / ,._| |  | '
+ | \__ |__}-|__}
+  \___)`*/
